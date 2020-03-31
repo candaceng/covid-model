436599ebf24f5905ec7fba4352a71e08864bf8f8
candaceng <50592521+candaceng@users.noreply.github.com>
2020-03-31 17:23:26 -0400

    Format changes

--- src/Profile.js
+++ src/Profile.js
@@ -44,39 +44,40 @@ export default class Profile extends Component {
 				We hear the phrase "flatten the curve" a lot, but what does that really mean? Inspired by the YouTube channel Numberphile, this article explores a 
 				method of modelling the Covid-19 virus. In the video, differential equations are plugged into a free statistical software to visualize and 
 				understand what this curve is all about. You can watch the video below or read through the article (or both!).
+				<i><br></br><br></br>Disclaimer: I am not an epidemiologist; this article is for informational purposes only.</i>
 			</p>
-            <iframe width="500" height="300"
+			<iframe width="500" height="300"
 				src="https://www.youtube.com/embed/k6nLfCbAzgo">
-		    </iframe>
+			</iframe>
 			<p className="walkthrough">
 				It is common in epidemiology for the SIR model to be used. The acronym represents three variables: 
 				<ul className="walkthrough">
-					<li><span id="variable">S</span> = the number of people susceptible to the disease</li>
-					<li><span id="variable">I</span> = the number of people infected</li>
-					<li><span id="variable">R</span> = the number of recovered people</li>
+					<li><b>S</b> = the number of people susceptible to the disease</li>
+					<li><b>I</b> = the number of people infected</li>
+					<li><b>R</b> = the number of recovered people</li>
 				</ul>
 				It is important to note that a "recovered" person in this context means that the person is no longer contagious, and deaths do fall under this category.
 				You can think of R standing for "removed" instead. From here we define three more variables, each of which represents the initial values of the SIR 
 				variables (SStart, IStart, and RStart). We assume that at time = 0, 1% of the population is infected, leaving the remaining 99% susceptible. The number
 				of recovered patients would start at 0 for obvious reasons. So we have, 
 				<ul className="walkthrough">
-					<li><span id="variable">SStart</span> = 0.99</li>
-					<li><span id="variable">IStart</span> = 0.01</li>
-					<li><span id="variable">RStart</span> = 0</li>
-					<li>time <span id="variable">t</span> as the independent variable</li>
+					<li><b>SStart</b> = 0.99</li>
+					<li><b>IStart</b> = 0.01</li>
+					<li><b>RStart</b> = 0</li>
+					<li>time <b>t</b> as the independent variable</li>
 				</ul>
-				We then define two adjustable constants that will aid in the derivation of the equations. The first one of which is <span id="variable">transm</span>, 
-				which represents the transmission rate of the disease. The equation <span id="variable">S'(t, S, I, R)</span> denotes the rate of change of S with respect 
+				We then define two adjustable constants that will aid in the derivation of the equations. The first one of which is <b>transm</b>, 
+				which represents the transmission rate of the disease. The equation <b>S'(t, S, I, R)</b> denotes the rate of change of S with respect 
 				to time which is dependent on the variables t, S, I, and R. A reasonable way to express this rate would be to multiply the transmission rate with the 
 				number of susceptible and infected people. The rate would also be negative because the number of susceptible people is expected to decrease as more
-				of them become infected. The second constant we introduce is <span id="variable"> recov</span>, which represents the recovery rate of infected patients.
+				of them become infected. The second constant we introduce is <b> recov</b>, which represents the recovery rate of infected patients.
 				Naturally, the rate of change of R with respect to time would be the number of infected people multiplied by the rate of recovery. The last equation,
 				the rate of change of I with respect to time, would be the rate at which people are getting infected minus the rate at which people are recovering.
 				Summarizing the equations, we have,
 				<ul className="walkthrough">
-					<li><span id="variable">S'(t, S, I, R) </span>= -(transm)(S)(I)</li>
-					<li><span id="variable">I'(t, S, I, R) </span>= (transm)(S)(I) - (recov)(I)</li>
-					<li><span id="variable">R'(t, S, I, R) </span>= (recov)(I)</li>
+					<li><b>S'(t, S, I, R) </b>= -(transm)(S)(I)</li>
+					<li><b>I'(t, S, I, R) </b>= (transm)(S)(I) - (recov)(I)</li>
+					<li><b>R'(t, S, I, R) </b>= (recov)(I)</li>
 				</ul>
 				Solving these three differential equations gives the curves shown below. Just for the purpose of visualization, the transmission rate was arbitrarily
 				set to 3.2, the recovery rate was set to 0.23, and time ranged from 0 to 20. Note that the units of these values do not matter as we are only concerned 
